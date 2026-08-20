08ccc770208cd2f92224ea074a6719bccbb2f99b
Dipratna29 <dipratnakamble@gmail.com>
2026-08-20 16:58:36 +0530

    Time: 1 ms (98.08%) | Memory: 46.7 MB (42.79%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,15 +1,24 @@
 class Solution {
-  public int[] resultArray(int[] nums) {
-    List<Integer> arr1 = new ArrayList<>(List.of(nums[0]));
-    List<Integer> arr2 = new ArrayList<>(List.of(nums[1]));
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+        int[] a = new int[n];
+        int[] b = new int[n];
 
-    for (int i = 2; i < nums.length; ++i)
-      if (arr1.get(arr1.size() - 1) > arr2.get(arr2.size() - 1))
-        arr1.add(nums[i]);
-      else
-        arr2.add(nums[i]);
+        int x = 1, y = 1;
+        a[0] = nums[0];
+        b[0] = nums[1];
 
-    arr1.addAll(arr2);
-    return arr1.stream().mapToInt(Integer::intValue).toArray();
-  }
+        for (int i = 2; i < n; i++) {
+            if (a[x - 1] > b[y - 1])
+                a[x++] = nums[i];
+            else
+                b[y++] = nums[i];
+        }
+
+        int[] ans = new int[n];
+        System.arraycopy(a, 0, ans, 0, x);
+        System.arraycopy(b, 0, ans, x, y);
+
+        return ans;
+    }
 }
